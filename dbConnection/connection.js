ae33776777512046b8d86c3a994276468f6e7f65
Santhosh Murthy <50907223+santhoshmurthybk@users.noreply.github.com>
2021-02-13 18:40:17 +0530

    Update connection.js

--- dbConnection/connection.js
+++ dbConnection/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb+srv://dbUser:dbUser@cluster0.jznsq.mongodb.net/book-list?retryWrites=true&w=majority';
+const URI = 'mongodb+srv://<username>:<password>@cluster0.jznsq.mongodb.net/book-list?retryWrites=true&w=majority';
 
 const connectDB = async () => {
     await mongoose.connect(URI, {
